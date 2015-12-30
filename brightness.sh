804242e8eaaa1d5aacd03da54e073b60e92fd0aa
ypcrts <32625281+ypcrts@users.noreply.github.com>
2015-12-30 02:36:49 -0500

    enhance

--- brightness.sh
+++ brightness.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
+if [ -z "$1" ]; then
+  return 1;
+fi
+TIME=300
 case $1 in
   up)
-    DISPLAY=:0 /usr/bin/xbacklight -inc 10% -time 300
+    INC="10"
     ;;
   down)
-    DISPLAY=:0 xbacklight -dec 10% -time 300 
+    INC="-10"
+    ;;
+  *)
+    INC="${1}"
+    if [ "$INC" -lt 5 ] && [ "$INC" -gt -5 ]; then
+      TIME=10
+    elif [ "$INC" -lt 10 ] && [ "$INC" -gt -10 ]; then
+      TIME=100
+    fi
     ;;
 esac
-sleep 300ms
-sudo -u gxg DISPLAY=:0 notify-send --urgency=low --expire-time=500 "Backlight: $(DISPLAY=:0 xbacklight -get | cut -d . -f 1)%" 
+DISPLAY=:0 /usr/bin/xbacklight -inc ${INC}% -time ${TIME} 
+sudo -u gxg DISPLAY=:0 notify-send --urgency=low --expire-time=500 "Backlight: $(DISPLAY=:0 xbacklight -get | cut -d . -f 1)%"
+return 0
